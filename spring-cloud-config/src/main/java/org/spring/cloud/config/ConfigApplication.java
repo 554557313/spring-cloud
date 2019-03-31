04508d954fe5760efce1b064012570764be3eb6c
guohuwei <554557313@qq.com>
2019-03-31 14:36:29 +0800

    test

--- spring-cloud-config/src/main/java/org/spring/cloud/config/ConfigApplication.java
+++ spring-cloud-config/src/main/java/org/spring/cloud/config/ConfigApplication.java
@@ -1,13 +1,14 @@
 package org.spring.cloud.config;
 
-/**
- * Hello world!
- *
- */
-public class ConfigApplication 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
     }
 }
