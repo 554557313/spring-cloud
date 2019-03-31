439a216b7b80f4c9c8392984199cfb9e6dcef2b6
guohuwei <554557313@qq.com>
2019-03-31 14:08:18 +0800

    commit

---
+++ spring-cloud-config/src/main/java/org/spring/cloud/config/ConfigApplication.java
@@ -0,0 +1,13 @@
+package org.spring.cloud.config;
+
+/**
+ * Hello world!
+ *
+ */
+public class ConfigApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
